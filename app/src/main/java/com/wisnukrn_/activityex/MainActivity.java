71b3036050e72e316b34ac084694e5bc1c557258
wisnukurniawan <wisnuko13@gmail.com>
2016-12-28 09:17:07 +0700

    learn fragment

--- app/src/main/java/com/wisnukrn_/activityex/MainActivity.java
+++ app/src/main/java/com/wisnukrn_/activityex/MainActivity.java
@@ -3,12 +3,20 @@ package com.wisnukrn_.activityex;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import static com.wisnukrn_.activityex.Constans.DEFAULT_STRING;
 import static com.wisnukrn_.activityex.Constans.PREF_FILE;
@@ -24,6 +32,11 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    MenuItem mMenuItem;
+    DrawerLayout mDrawerLayout;
+
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,14 +44,36 @@ public class MainActivity extends AppCompatActivity {
 
         logData("onCreate Called");
 
-        komentarField = (EditText) findViewById(R.id.komentar);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_main);
         toolbar.setTitle(getResources().getString(R.string.app_name));
         toolbar.setPadding(0, getStatusBarHeight(), 0, 0);
         setSupportActionBar(toolbar);
 
-        loadString();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.dl_main);
+        final NavigationView navigationView = (NavigationView) findViewById(R.id.nv_main);
+
+        setNavListener(navigationView);
+        addNavListener(toolbar, navigationView);
+
+        mDrawerLayout.addDrawerListener(mActionBarDrawerToggle);
+//        loadString();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (mDrawerLayout.isDrawerVisible(GravityCompat.START)){
+            mDrawerLayout.closeDrawers();
+        } else {
+            finish();
+        }
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mActionBarDrawerToggle.syncState();
+        showHomeFragment();
     }
 
     @Override
@@ -60,8 +95,7 @@ public class MainActivity extends AppCompatActivity {
         super.onPause();
 
         logData("onPause Called");
-        String komentar = komentarField.getText().toString();
-        saveString(komentar);
+//        saveString(komentar);
     }
 
     @Override
@@ -82,9 +116,86 @@ public class MainActivity extends AppCompatActivity {
         final int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
         return resourceId > 0 ? getResources().getDimensionPixelSize(resourceId) : 0;
     }
-    
+
+    private void addNavListener(final Toolbar toolbar, final NavigationView navigationView) {
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                toolbar, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                if (null != mMenuItem){
+                    switchNavigationMenu(mMenuItem.getItemId());
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                super.onDrawerSlide(drawerView, slideOffset);
+
+                if (null != drawerView && drawerView == navigationView){
+                    super.onDrawerSlide(drawerView, 0);
+                } else {
+                    super.onDrawerSlide(drawerView, slideOffset);
+                }
+            }
+        };
+    }
+
+    private void setNavListener(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                mMenuItem = item;
+                item.setCheckable(true);
+                mDrawerLayout.closeDrawers();
+
+                return true;
+            }
+        });
+    }
+
+    private void switchNavigationMenu(int itemId) {
+        switch (itemId){
+            case R.id.menu_home: {
+                showHomeFragment();
+                break;
+            }
+            case R.id.menu_detail:{
+                showDetailFragment();
+                break;
+            }
+        }
+    }
+
+    private void showHomeFragment() {
+        final String tags = HomeFragment.class.getSimpleName();
+
+        if (getSupportFragmentManager().findFragmentByTag(tags) == null){
+            final Fragment fragment = new HomeFragment();
+            final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fl_main, fragment, tags);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void showDetailFragment() {
+        final String tags = DetailFragment.class.getSimpleName();
+
+        if (getSupportFragmentManager().findFragmentByTag(tags) == null) {
+            final Fragment fragment = new DetailFragment();
+            final FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fl_main, fragment, tags);
+            fragmentTransaction.commit();
+        }
+    }
+
     void logData(String data){
-        Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+//        Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
         Log.d(TAG, data);
     }
 
